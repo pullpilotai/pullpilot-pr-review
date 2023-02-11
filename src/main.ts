5bc3e28d64904494013b5c2df90b62b764dfba5d
Pull Pilot AI <pullpilotai@gmail.com>
2023-02-11 09:30:51 +0100

    latest

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function run(): Promise<void> {
     const pull_pilot_retry_seconds = core.getInput("pull_pilot_retry", { required: false }) || 20;
 
     //const baseUrl = "https://api.pullpilot.ai";
-    const baseUrl = "https://1011-2001-4bb8-190-6e43-8078-c8f9-bf1e-2293.eu.ngrok.io/"
+    const baseUrl = "https://1011-2001-4bb8-190-6e43-8078-c8f9-bf1e-2293.eu.ngrok.io"
 
     const client = getOctokit(github_token);
 
